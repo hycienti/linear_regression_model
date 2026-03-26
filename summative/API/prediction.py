1407c7a161b9d390206f080f0a341ec600f86f8b
Hycient Ayanaisiuwa Igweze <h.igweze@alustudent.com>
2026-03-26 14:10:08 +0000

    chore(cors): enable just port 3000

--- summative/API/prediction.py
+++ summative/API/prediction.py
@@ -10,9 +10,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], #TODO:  Change it later to allow specific origins
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["POST"],
     allow_headers=["*"],
 )
 
